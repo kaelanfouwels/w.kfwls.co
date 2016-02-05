30c179e92d74562140a5242f4da970d481a3fc61
Kaelan Fouwels <kae@kfwls.co>
2016-02-05 12:59:19 +0000

    update

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,20 +1,16 @@
-var duration = 250;
+var duration = 25000;
 var currentEscapeDialog;
 
-function SetOpa(element, o) {
-	element.style.opacity = o;
-}
-
-function FadeOut(element) {
+function fOut(element) {
 	setTimeout(element + ".style.display = 'none'", duration);
-	for (i = 0; i <= 1; i += 0.01) {
-		setTimeout(SetOpa(element, 1 - i), i * duration);
+	for (i = 1; i >= 0; i -= 0.01) {
+		setTimeout(element.style.opacity = i, i * duration);
 	}
 }
-function FadeIn(element) {
+function fIn(element) {
 	element.style.display = 'block';
 	for (i = 0; i <= 1; i += 0.01) {
-		setTimeout(SetOpa(element,i), i * duration);
+		setTimeout(element.style.opacity = i, i * duration);
 	}
 }
 
@@ -22,14 +18,14 @@ function toggleDialog(id, show) {
 	var element = document.getElementById(id);
 	currentEscapeDialog = element;
 	if (show) {
-		FadeIn(element);
+		fIn(element);
 	}
 	else {
-		FadeOut(element);
+		fOut(element);
 	}
 }
 function keyhandler(e) {
 	if (e.keyCode == 27)
-		FadeOut(currentEscapeDialog);
+		fOut(currentEscapeDialog);
 }
 document.onkeydown = keyhandler;
