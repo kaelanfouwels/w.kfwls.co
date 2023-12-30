9b7beab20178437e3692357fc8f417dd6590020a
Kaelan Thijs Fouwels <kaelan.thijs@fouwels.com>
2023-12-30 09:01:41 +0000

    feat: networkInfo (#17)

---
+++ lib/exp/browser.tsx
@@ -0,0 +1,54 @@
+// SPDX-FileCopyrightText: 2023 Kaelan Thijs Fouwels <kaelan.thijs@fouwels.com>
+//
+// SPDX-License-Identifier: MIT
+
+// Types for experimental browser constructs not yet in typescript core
+
+// https://wicg.github.io/netinfo/#-dfn-networkinformation-dfn-interface
+export interface NetworkInformation {
+  type: string
+  effectiveType: string
+  downlink: number
+  downlinkMax: number
+  rtt: number
+};
+
+// https://wicg.github.io/local-font-access/
+export interface FontData {
+  postscriptName: string
+  fullName: string
+  family: string
+  style: string
+}
+
+// https://wicg.github.io/local-font-access/
+export async function windowQueryLocalFonts (): Promise<FontData[] | undefined> {
+  let localfonts: FontData[] | undefined
+  try {
+    localfonts = (await (window as any).queryLocalFonts() as FontData[])
+  } catch (e: any) {
+    console.debug('exp::queryLocalFonts: failed to grab local fonts:' + String(e))
+  }
+
+  if (localfonts === undefined) {
+    return undefined
+  }
+
+  return localfonts.map((x) => x)
+}
+
+// https://wicg.github.io/netinfo/#-dfn-networkinformation-dfn-interface
+export function navigationConnection (): NetworkInformation | undefined {
+  const conn = ((navigator as any)?.connection as NetworkInformation)
+  if (conn === undefined) {
+    return undefined
+  }
+
+  return {
+    type: conn.type,
+    effectiveType: conn.effectiveType,
+    downlink: conn.downlink,
+    downlinkMax: conn.downlinkMax,
+    rtt: conn.rtt
+  }
+}
